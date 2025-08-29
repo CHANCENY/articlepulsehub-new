6314b45636f1c0776b0f4dc28c09a04fc871f866
CHANCENY <nyasuluchance6@gmail.com>
2025-08-29 19:29:49 +0530

    po

--- news.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . "/vendor/autoload.php";
-
-\Simp\Core\lib\app\App::consoleApp();;
-
-$messages = \Simp\Public\Module\news_org_api\src\Plugin\NewsLiveApi::cronHandler();
-
-print_r($messages);
